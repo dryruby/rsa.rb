33b947325161dc45039be228be22faab71a976af
Arto Bendiken <arto@bendiken.net>
2010-09-05 02:13:59 +0200

    Implemented the RSA::PKCS1.i2osp method.

--- lib/rsa/pkcs1.rb
+++ lib/rsa/pkcs1.rb
@@ -1,12 +1,48 @@
 module RSA
   ##
-  # Support for the PKCS#1 (aka RFC 3447) padding schemes.
+  # Support for the PKCS #1 (aka RFC 3447) padding schemes.
   #
   # @see http://en.wikipedia.org/wiki/PKCS1
-  # @see http://en.wikipedia.org/wiki/OAEP
   # @see http://tools.ietf.org/html/rfc3447
   # @see http://www.rsa.com/rsalabs/node.asp?id=2125
   module PKCS1
-    # TODO
+    ##
+    # Converts a nonnegative integer to an octet string of a specified
+    # length.
+    #
+    # This is the {RSA::PKCS1 PKCS #1} I2OSP (Integer-to-Octet-String)
+    # primitive.
+    #
+    # Refer to PKCS #1 v2.1, section 4.1, p. 8.
+    #
+    # @param  [Integer] x
+    # @param  [Integer] len
+    # @return [String]
+    # @see    http://tools.ietf.org/html/rfc3447#section-4.1
+    # @raise  [ArgumentError] if `n` is greater than 256^len
+    def self.i2osp(x, len)
+      raise ArgumentError, "integer too large" if x >= 256**len
+
+      len.downto(1).inject(String.new) do |s, i|
+        b = (x & 0xFF).chr
+        x >>= 8
+        s = b + s
+      end
+    end
+
+    ##
+    # Converts an octet string to a nonnegative integer.
+    #
+    # This is the {RSA::PKCS1 PKCS #1} OS2IP (Octet-String-to-Integer)
+    # primitive.
+    #
+    # Refer to PKCS #1 v2.1, section 4.2, p. 9.
+    #
+    # @param  [String] x
+    # @return [Integer]
+    # @see    http://tools.ietf.org/html/rfc3447#section-4.2
+    def self.os2ip(x)
+      # TODO
+    end
   end # module PKCS1
 end # module RSA
--- spec/pkcs1_spec.rb
+++ spec/pkcs1_spec.rb
@@ -1,5 +1,23 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe RSA::PKCS1 do
-  # TODO
+  context "RSA::PKCS1.i2osp" do
+    it "raises an error if x >= 256**len" do
+      lambda { RSA::PKCS1.i2osp(256**3, 2) }.should raise_error(ArgumentError)
+      lambda { RSA::PKCS1.i2osp(9_202_000, 2) }.should raise_error(ArgumentError)
+    end
+
+    it "returns the correct octet string" do
+      RSA::PKCS1.i2osp(9_202_000, 3).should == "\x8C\x69\x50"
+    end
+
+    it "inserts zero padding as needed" do
+      RSA::PKCS1.i2osp(9_202_000, 4).should == "\x00\x8C\x69\x50"
+      RSA::PKCS1.i2osp(9_202_000, 5).should == "\x00\x00\x8C\x69\x50"
+    end
+  end
+
+  context "RSA::PKCS1.os2ip" do
+    # TODO
+  end
 end
